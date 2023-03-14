74c391c0ed758f4a8b2542d3beac768b47dd0fc2
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-14 16:51:27 +0300

    Task1 Fix of variable paper

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
         System.out.println("Задача 1");
         var dog = 8.0;
         var cat = 3.6;
-        var paper = 7639;
+        var paper = 763789;
+
+
+
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
