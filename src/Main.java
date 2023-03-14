109ff9138eae3fbddfa35cad03cc92fffa96b9c9
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-14 16:54:36 +0300

    Task2 Addition for all vars

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
         System.out.println("Задача 1");
         var dog = 8.0;
         var cat = 3.6;
-        var paper = 7639;
+        var paper = 763789;
+
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
