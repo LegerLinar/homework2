b51c97217b3ff39380158cd4a4b51294062afbf2
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-14 17:17:47 +0300

    Part2 New variables. Multiplication and division. Rename of tasks-separating methods.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        part1();
+        part2();
     }
 
-    public static void task1 () {
+    public static void part1() {
         System.out.println("Задача 1");
+
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 2");
 
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        System.out.println("Задача 3");
 
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
-
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
     }
+
+    public static void part2() {
+        System.out.println("Задача 4");
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+    }
+
 }
